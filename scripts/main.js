a614a73134d5201a40392eb57b2fe5173069472b
Reinaert Van de Cruys <reinaert.vandecruys@gmail.com>
2016-12-03 23:16:18 +0100

    Implement basic flow functionality.

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,20 @@ SyncStates = {
 };
 
 
+/* Main flow of the tax report */
+var flow = [
+    "Vraag 1/5",
+    "Vraag 2/5",
+    "Vraag 3/5",
+    "Vraag 4/5",
+    "Vraag 5/5"
+];
+
+
+/* Completed parts of the flow */
+var completed = [];
+
+
 /* Set the synchronization state */
 function setSyncState(state) {
     var syncState = $('#sync-state');
@@ -78,8 +92,72 @@ function setProgress(fraction) {
 }
 
 
+/* Update the page to match the current state of the tax report */
+function renderPage() {
+    var content = $('#content');
+    var question;
+
+    if (flow.length > 0) {
+        question = flow[0];
+    } else {
+        question = 'Klaar!';
+    }
+
+    setProgress(completed.length / (flow.length + completed.length));
+    content.empty();
+    content.append('<span class="question">' + question + '</span>');
+}
+
+
+/* Return to the previous page */
+function previousPage() {
+    if (flow.length > 0) {
+        var page = flow.pop();
+        flow.unshift(page);
+        renderPage();
+    }
+}
+
+
+/* Mark the current page as solved and go to the next page */
+function nextPage() {
+    if (flow.length > 0) {
+        var page = flow.shift();
+        completed.push(page);
+        renderPage();
+    }
+}
+
+
+/* Jump to the next page without resolving the current page */
+function skipPage() {
+    if (flow.length > 0) {
+        var page = flow.shift();
+        flow.push(page);
+        renderPage();
+    }
+}
+
+
+$('#action-button-previous')[0].addEventListener('click', function () {
+    previousPage();
+});
+
+$('#action-button-yes')[0].addEventListener('click', function () {
+    nextPage();
+});
+
+$('#action-button-no')[0].addEventListener('click', function () {
+    nextPage();
+});
+
+$('#action-button-skip')[0].addEventListener('click', function () {
+    skipPage();
+});
+
+
 /* Initialize the interface */
 setSyncState(SyncStates.SYNCED);
 setUserNames(['Erik Tienen', 'Tine van de Meent']);
 setCurrentStep(1);
-setProgress(0.05);
+renderPage();
